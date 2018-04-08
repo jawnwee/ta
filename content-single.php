8019c96e218f362cfdbbf50ede0f80f5ed566749
John <jy.johnlee@gmail.com>
2018-04-07 20:02:35 -0700

    remove comments

--- content-single.php
+++ content-single.php
@@ -22,13 +22,6 @@
 
 		<?php silvia_related_posts(); // Display the related posts. ?>
 
-		<?php
-			// If comments are open or we have at least one comment, load up the comment template
-			if ( comments_open() || '0' != get_comments_number() ) :
-				comments_template();
-			endif;
-		?>
-
 	</div>
 
 	<div class="entry-meta">
