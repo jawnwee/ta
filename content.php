4397b24ca754825bc0441e649c28739674099647
John <jy.johnlee@gmail.com>
2018-06-02 16:43:41 -0700

    test

--- content.php
+++ content.php
@@ -9,15 +9,15 @@ $size = '';
 $layout = silvia_mod( $prefix . 'grid-layout' );
 
 ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> <?php hybrid_attr( 'post' ); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('front-page'); ?> <?php hybrid_attr( 'post' ); ?>>
 
 	<?php if ( has_post_thumbnail() ) : ?>
 		<a class="thumbnail-link" href="<?php the_permalink(); ?>">
 			<div class="thumbnail-container">
 				<?php the_post_thumbnail( 'full', array( 'class' => 'entry-thumbnail', 'alt' => esc_attr( get_the_title() ) ) ); ?>
-			</div>
-			<div class="entry-container">
+				<div class="entry-container">
 				<?php the_title( sprintf( '<h2 class="entry-title" ' . hybrid_get_attr( 'entry-title' ) . '><a href="%s" rel="bookmark" itemprop="url">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+			</div>
 			</div>
 		</a>
 	<?php endif; ?>
