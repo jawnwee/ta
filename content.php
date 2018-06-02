8adf653b9cd2a3d15f30e25ac1e01f721252f838
John <jy.johnlee@gmail.com>
2018-06-02 15:48:57 -0700

    test

--- content.php
+++ content.php
@@ -12,6 +12,7 @@ $layout = silvia_mod( $prefix . 'grid-layout' );
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> <?php hybrid_attr( 'post' ); ?>>
 
 	<?php if ( has_post_thumbnail() ) : ?>
+		<a class="thumbnail-link" href="<?php the_permalink(); ?>">
 			<div class="thumbnail-container">
 				<?php the_post_thumbnail( 'full', array( 'class' => 'entry-thumbnail', 'alt' => esc_attr( get_the_title() ) ) ); ?>
 				<div class="entry-container">
@@ -26,6 +27,7 @@ $layout = silvia_mod( $prefix . 'grid-layout' );
 					<?php endif; // End if categories ?>
 				<?php endif; ?>
 			</div>
+		</a>
 	<?php endif; ?>
 	
 </article><!-- #post-## -->
